bc25152cc3f2de22971d5434c84c90cd33ba73c2
Angel Castillo <82915179+Angel-113@users.noreply.github.com>
2026-01-27 13:59:30 -0600

    Delete build directory

--- build/CMakeFiles/malloc_core.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for malloc_core.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/angel/Projects/allocator
-# Build directory: /home/angel/Projects/allocator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[MainTest]=] "/home/angel/Projects/allocator/build/run_tests")
-set_tests_properties([=[MainTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/angel/Projects/allocator/CMakeLists.txt;33;add_test;/home/angel/Projects/allocator/CMakeLists.txt;0;")
